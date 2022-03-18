6b4f59c4a03d2e0bd8150cc15dfd1cfe1e7d112e
Enzo Muñoz <enzom.dev@gmail.com>
2022-03-18 12:04:47 -0300

    Cleaning more props now that I'm using ContextAPI

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,65 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { TasksContext } from '../../context/TasksContext'
 import { Flex, useToast, Button } from '@chakra-ui/react'
 import { DeleteIcon } from '@chakra-ui/icons'
 import EditTaskModal from '../Modals/EditTaskModal'
 
-const Items = ({ name, id, tasks, deleteTaskHandler }) => {
-  const toast = useToast()
-  const [currentTask, setCurrentTask] = useState(name)
+const Items = ({ name, id }) => {
+    const toast = useToast()
+    const { userTasks, deleteTaskHandler } = useContext(TasksContext)
 
-  const deletedTaskToast = () => {
-    toast({
-      title: 'Task deleted.',
-      description: `The task ${name} was deleted.`,
-      status: 'info',
-      duration: 1000,
-      isClosable: true
-    })
-  }
+    const deletedTaskToast = () => {
+        toast({
+            title: 'Task deleted.',
+            description: `The task ${name} was deleted.`,
+            status: 'info',
+            duration: 1000,
+            isClosable: true
+        })
+    }
 
-  /*
-   * Handles the delete task feature.
-   * It filters the current tasks array and returns all tasks except the current task using the id.
-   * It's using the deleteTaskHandler function received via props from ItemsContainer and passed
-   * from Container.jsx.
-   * deleteTaskHandler function prop updates the state with the new tasks after being filtered.
-   */
-  const deleteTask = () => {
-    const filteredTasks = tasks.filter((task) => {
-      return task.id !== id
-    })
-    deleteTaskHandler(filteredTasks)
-    deletedTaskToast()
-  }
+    /*
+     * Handles the delete task feature.
+     * It filters the current tasks array and returns all tasks except the current task using the id.
+     * It's using the deleteTaskHandler function received via props from ItemsContainer and passed
+     * from Container.jsx.
+     * deleteTaskHandler function prop updates the state with the new tasks after being filtered.
+     */
+    const deleteTask = () => {
+        const filteredTasks = userTasks.filter((task) => {
+            return task.id !== id
+        })
+        deleteTaskHandler(filteredTasks)
+        deletedTaskToast()
+    }
 
-  return (
-    <>
-      <Flex
-        as="li"
-        w={'100%'}
-        bg="cyan.300"
-        mb={1}
-        p={2}
-        fontSize="lg"
-        id={id}
-        justify="space-between"
-        align="center"
-      >
-        {currentTask}
-        <Flex gap="2">
-          <EditTaskModal taskId={id} taskName={name} />
-          <Button
-            type="button"
-            onClick={deleteTask}
-            bg="bgRed.900"
-            _hover={{ bg: 'bgRed.dark' }}
-          >
-            <DeleteIcon color="white" />
-          </Button>
-        </Flex>
-      </Flex>
-    </>
-  )
+    return (
+        <>
+            <Flex
+                as="li"
+                w={'100%'}
+                bg="cyan.300"
+                mb={1}
+                p={2}
+                fontSize="lg"
+                id={id}
+                justify="space-between"
+                align="center"
+            >
+                {name}
+                <Flex gap="2">
+                    <EditTaskModal taskId={id} taskName={name} />
+                    <Button
+                        type="button"
+                        onClick={deleteTask}
+                        bg="bgRed.900"
+                        _hover={{ bg: 'bgRed.dark' }}
+                    >
+                        <DeleteIcon color="white" />
+                    </Button>
+                </Flex>
+            </Flex>
+        </>
+    )
 }
 
 export default Items
